d11aecab8b19e4b741f1ba4bb9634fdd0e827e0c
nszoke <nszoke@ENDAUTOELRNNWWV>
2024-11-28 13:27:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Channels;
+
+namespace ConsoleAppETA25
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello, World!");+
+
+     /*       int number = 10;
+            const double Pi = 3.14159;
+            Console.WriteLine(number);  
+            Console.WriteLine(Pi);
+
+            int i = default;
+            float f = default;*/
+
+            //Console.WriteLine(i);
+
+            //Console.WriteLine("Ce varsta ai?");
+            //int varsta = Convert.ToInt16(Console.ReadLine());
+
+            //Console.WriteLine("Din ce oras esti?");
+            //string oras = Console.ReadLine();
+
+            //Console.WriteLine("Ai fost la vot?");
+            //bool aVotat = Convert.ToBoolean(Console.ReadLine());
+
+
+
+            //Console.WriteLine("Varsta introdusa de tine este: " + varsta);
+            //Console.WriteLine("Orasul introdus de tine: " + oras);
+
+            //Console.WriteLine("Varsta si orasul introduse de tine este: {0},{1}", varsta, oras);
+            //Console.WriteLine($"Varsta introdusa de tine este: {varsta}");
+
+            //Console.WriteLine($"Varsta si Orasul si votul introduse de tine sunt: {varsta}, {oras}, {aVotat}");
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
