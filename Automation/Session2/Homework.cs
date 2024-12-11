0027ace657839c0e84f48bde42573b8141095a4f
ENDAVA\Nszoke <szokenoemi@yahoo.com>
2024-12-11 21:11:07 +0200

    extra practice + homework_DeleteAllElementsFromTable

---
+++ Automation/Session2/Homework.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ConsoleAppETA25.Automation.Session2
+{
+    public class Homework
+    {
+        /*
+        Stergeti toate elementele din tabel inainte de a adauga un rand nou 
+        - folosind pasi similari cu cei din curs.
+        Sa identificati textul afisat atunci cand nu mai exista elemente in tabel.
+        EXTRA:  Editati un rand existent si salvati-l.*/
+
+
+
+
+        public IWebDriver Driver;
+        public const string BaseUrl = "https://demoqa.com/";
+
+
+        public const string firstName = "Noemi";
+        public const string lastName = "Sz";
+        public const string email = "ns-test@email.com";
+        public const string age = "99";
+        public const string salary = "999999";
+        public const string department = "Software dev";
+
+
+
+        [SetUp]
+
+        public void Setup()
+        {
+            Driver = new ChromeDriver();
+            Driver.Navigate().GoToUrl(BaseUrl);
+            Driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+
+        public void DeletAllElementsFromTable()
+        {
+
+            IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
+            js.ExecuteScript("window.scrollTo(0,500)");
+
+            IWebElement elementButton = Driver.FindElement(By.XPath("//div[@class='card mt-4 top-card'][1]"));
+            elementButton.Click();
+
+            IWebElement webTableButton = Driver.FindElement(By.XPath("//span[text()='Web Tables']"));
+            webTableButton.Click();
+
+            IWebElement deleteButton1 = Driver.FindElement(By.Id("delete-record-1"));
+            deleteButton1.Click();
+
+            IWebElement deleteButton2 = Driver.FindElement(By.Id("delete-record-2"));
+            deleteButton2.Click();
+
+            IWebElement deleteButton3 = Driver.FindElement(By.Id("delete-record-3"));
+            deleteButton3.Click();
+
+            //
+            //WebElement deleteButtonOtherWay = Driver.FindElement(By.XPath("(//span[@title='Delete'])[1] | (//span[@title='Delete'])[2] | (//span[@title='Delete'])[3]"));
+            //deleteButtonOtherWay.Click();   
+
+
+            //identify the text 
+            IWebElement noRowsFound = Driver.FindElement(By.XPath("//div[@class='rt-noData']"));
+
+
+            IWebElement addButton = Driver.FindElement(By.Id("addNewRecordButton"));
+            addButton.Click();
+
+            IWebElement FirstName = Driver.FindElement(By.Id("firstName"));
+            FirstName.SendKeys(firstName);
+
+            IWebElement LastName = Driver.FindElement(By.Id("lastName"));
+            LastName.SendKeys(lastName);
+
+            IWebElement Email = Driver.FindElement(By.Id("userEmail"));
+            Email.SendKeys(email);
+
+            IWebElement Age = Driver.FindElement(By.Id("age"));
+            Age.SendKeys(age);
+
+            IWebElement Salary = Driver.FindElement(By.Id("salary"));
+            Salary.SendKeys(salary);
+
+            IWebElement Department = Driver.FindElement(By.Id("department"));
+            Department.SendKeys(department);
+
+            IWebElement SubmitButton = Driver.FindElement(By.Id("submit"));
+            SubmitButton.Click();
+
+            // edit the first row -BasicScenario
+
+            IWebElement editRow = Driver.FindElement(By.Id("edit-record-1"));
+            editRow.Click();
+
+            IWebElement FirstNameEdited = Driver.FindElement(By.Id("firstName"));
+            FirstNameEdited.SendKeys("_blabla");
+
+            IWebElement SubmitButton2 = Driver.FindElement(By.Id("submit"));
+            SubmitButton2.Click();
+
+
+            //edit the row where FirstName is Noemi
+
+            //IWebElement editRowBasedOnColumn = Driver.FindElement(By.XPath("//div[@class='rt-td'][1][contains(text(),'Noemi')]"));
+            //need help :)
+
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Automation/Session2/WebTableEx2.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ConsoleAppETA25.Automation.Session2
+{
+    
+
+    public class WebTableEx2
+
+    {
+        IWebDriver Driver;
+
+      [Test]
+
+      public void TestMethod()
+        {
+
+            Driver = new ChromeDriver();
+            Driver.Navigate().GoToUrl("https://demoqa.com/");
+            Driver.Manage().Window.Maximize();
+
+            IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
+            js.ExecuteScript("window.scrollTo(0,500)");
+
+
+            IWebElement elementButton = Driver.FindElement(By.XPath("//div[@class='card mt-4 top-card'][1]"));
+            elementButton.Click();
+
+            IWebElement webTableButton = Driver.FindElement(By.XPath("//span[text()='Web Tables']"));
+            webTableButton.Click();
+
+            IWebElement addButton = Driver.FindElement(By.Id("addNewRecordButton"));
+            addButton.Click();
+
+            IWebElement firstName = Driver.FindElement(By.Id("firstName"));
+            firstName.SendKeys("Noe");
+
+            IWebElement lastName = Driver.FindElement(By.Id("lastName"));
+            lastName.SendKeys("Szzok");
+
+
+            IWebElement email = Driver.FindElement(By.Id("userEmail"));
+            email.SendKeys("test@test.com");
+
+            IWebElement age = Driver.FindElement(By.Id("age"));
+            age.SendKeys("99");
+
+            IWebElement salary = Driver.FindElement(By.Id("salary"));
+            salary.SendKeys("10000");
+
+            IWebElement department = Driver.FindElement(By.Id("department"));
+            department.SendKeys("IT");
+
+
+            //IJavaScriptExecutor jse = (IJavaScriptExecutor)Driver;
+            IWebElement submitButton = Driver.FindElement(By.Id("submit"));
+            submitButton.Click();
+            //submitButton.Submit();
+            //jse.ExecuteScript("arguments[0].click();," submitButton);
+
+            IWebElement newRowWebTable = Driver.FindElement(By.XPath("//div[@class='rt-tr-group'][4]"));
+
+            Console.WriteLine(newRowWebTable.Text);
+
+            //Assert.That(newRowWebTable.Text.Contains("Noe"));
+            //Assert.That(newRowWebTable.Text.Contains("Szzok"));
+            //Assert.That(newRowWebTable.Text.Contains("test@test.com"));
+            //Assert.That(newRowWebTable.Text.Contains("99"));
+            //Assert.That(newRowWebTable.Text.Contains("10000"));
+            //Assert.That(newRowWebTable.Text.Contains("IT"));
+
+            IWebElement firstname1 = Driver.FindElement(By.XPath("//div[@class='rt-tr-group'][4]//div[@class='rt-td'][1]"));
+            IWebElement lastname1 = Driver.FindElement(By.XPath("//div[@class='rt-tr-group'][4]//div[@class='rt-td'][2]"));
+            IWebElement age1 = Driver.FindElement(By.XPath("//div[@class='rt-tr-group'][4]//div[@class='rt-td'][3]"));
+            IWebElement email1 = Driver.FindElement(By.XPath("//div[@class='rt-tr-group'][4]//div[@class='rt-td'][4]"));
+            IWebElement salary1 = Driver.FindElement(By.XPath("//div[@class='rt-tr-group'][4]//div[@class='rt-td'][5]"));
+            IWebElement department1 = Driver.FindElement(By.XPath("//div[@class='rt-tr-group'][4]//div[@class='rt-td'][6]"));
+
+            Assert.That(firstname1.Text.Equals("Noe"));
+            Assert.That(lastname1.Text.Equals("Szzok"));
+            Assert.That(email1.Text.Equals("test@test.com"));
+            Assert.That(age1.Text.Equals("99"));
+            Assert.That(salary1.Text.Equals("10000"));
+            Assert.That(department1.Text.Equals("IT"));
+
+
+
+
+
+
+
+        }
+
+        /* [TearDown]
+
+         public void TearDown()
+         {
+             if (Driver != null)
+             {
+
+                 Driver.Dispose();
+                 Driver.Quit();
+             }
+         }
+
+         */
+
+
+    }
+}
